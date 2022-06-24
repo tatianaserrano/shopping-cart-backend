33b1fd1560891fbdb48d9a5e4223e9614cd91857
tatianaserano <tatianaserano96@gmail.com>
2022-06-24 15:37:31 -0500

    add try catch

--- src/controllers/coupons.js
+++ src/controllers/coupons.js
@@ -1,10 +1,15 @@
-const { executeQuery } = require("../services/mysql.service")
+const { executeQuery } = require("../services/mysql.service");
 
-const getCoupons = async(req, res) => {
-    const coupons = await executeQuery('SELECT * FROM coupons')
-    res.json(coupons)
-}
+const getCoupons = async (req, res) => {
+  try {
+    const coupons = await executeQuery("SELECT * FROM coupons");
+    res.json(coupons);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error });
+  }
+};
 
 module.exports = {
-    getCoupons
-}
+  getCoupons,
+};
--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,10 +1,15 @@
-const { executeQuery } = require("../services/mysql.service")
+const { executeQuery } = require("../services/mysql.service");
 
-const getProducts = async(req, res) => {
-    const products = await executeQuery('SELECT * FROM products')
-    res.json(products)
-}
+const getProducts = async (req, res) => {
+  try {
+    const products = await executeQuery("SELECT * FROM products");
+    res.json(products);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error });
+  }
+};
 
 module.exports = {
-    getProducts
-}
+  getProducts,
+};
